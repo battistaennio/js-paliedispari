4c12d5c0da3f6b1527828ceba57f8e2e7f9194dc
battistaennio <ennio.battista99@gmail.com>
2024-06-12 16:11:46 +0200

    feat: add function randomNumber and evenOdd

--- Pari e Dispari/main.js
+++ Pari e Dispari/main.js
@@ -1,3 +1,16 @@
-//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+//L’utente sceglie pari o dispari 
+//inserisce un numero da 1 a 5.
 //Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-//Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+//Sommiamo i due numeri 
+//Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+
+//funzione per generare un numero random
+function randomNumber(min, max) {
+    let number = Math.floor(Math.random() * (max - min + 1)) + min;
+    return number
+}
+
+//funzione per stabilire pari o dispari
+function evenOdd(num) {
+    return num % 2 === 0 ? "Pari" : "Dispari";
+}
