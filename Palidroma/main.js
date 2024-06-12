c9de31dc092f12f2a23e606b1f7535c2dbf99bd9
battistaennio <ennio.battista99@gmail.com>
2024-06-12 15:06:47 +0200

    Feat: add reverse cycle

--- Palidroma/main.js
+++ Palidroma/main.js
@@ -5,6 +5,16 @@
 const userWord = prompt("Inserisci una parola");
 
 //creo funzione
-function verificaParolaPalindroma(){
-    
+function verificaParolaPalindroma(word){  //se userWord al contario è uguale a userWord, la parola è palindroma
+    //dichiaro word al contrario
+    let reverseWord = "";
+    //creo ciclo per attraversare userWord al contario
+    for (let i = word.length - 1; i >= 0; i--) {
+        reverseWord += word[i];
+    }
+    console.log(reverseWord);
 }
+
+
+//risultato (per test)
+const result = verificaParolaPalindroma(userWord)
