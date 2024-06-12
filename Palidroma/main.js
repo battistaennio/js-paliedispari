85bc28a63a7090a7eef8de34bd2d229208fa8701
battistaennio <ennio.battista99@gmail.com>
2024-06-12 14:37:49 +0200

    Feat: ask user word + add function "verificaParolaPalindroma"

--- Palidroma/main.js
+++ Palidroma/main.js
@@ -1,2 +1,10 @@
 //Chiedere all’utente di inserire una parola
 //Creare una funzione per capire se la parola inserita è palindroma
+
+//chiedo parola all'utente
+const userWord = prompt("Inserisci una parola");
+
+//creo funzione
+function verificaParolaPalindroma(){
+    
+}
