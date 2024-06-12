8a40da73dc1a56228d6f9559ddbac85152833614
battistaennio <ennio.battista99@gmail.com>
2024-06-12 15:15:49 +0200

    Feat: add condition "palindroma si/no"

--- Palidroma/main.js
+++ Palidroma/main.js
@@ -6,15 +6,23 @@ const userWord = prompt("Inserisci una parola");
 
 //creo funzione
 function verificaParolaPalindroma(word){  //se userWord al contario è uguale a userWord, la parola è palindroma
-    //dichiaro word al contrario
+
+    //dichiaro word al contrario = 0
     let reverseWord = "";
+
     //creo ciclo per attraversare userWord al contario
     for (let i = word.length - 1; i >= 0; i--) {
+
+        //setto che reverseWord è composta dagli elementi di word al contrario
         reverseWord += word[i];
     }
-    console.log(reverseWord);
+
+    //se word è uguale a reverseWord è palindroma altrimenti no
+    return word == reverseWord ? "è palindroma" : "non è palindroma";
 }
 
 
-//risultato (per test)
+//risultato 
 const result = verificaParolaPalindroma(userWord)
+//(per test)
+console.log(result);
