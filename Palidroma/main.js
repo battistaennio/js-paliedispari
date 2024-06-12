ab838c6caad2818540e687c61b14a544d7e7345a
battistaennio <ennio.battista99@gmail.com>
2024-06-12 16:01:26 +0200

    Feat: add JS event on click and output

--- Palidroma/main.js
+++ Palidroma/main.js
@@ -1,9 +1,6 @@
 //Chiedere all’utente di inserire una parola
 //Creare una funzione per capire se la parola inserita è palindroma
 
-//chiedo parola all'utente
-const userWord = prompt("Inserisci una parola");
-
 //creo funzione
 function verificaParolaPalindroma(word){  //se userWord al contario è uguale a userWord, la parola è palindroma
 
@@ -18,11 +15,22 @@ function verificaParolaPalindroma(word){  //se userWord al contario è uguale a 
     }
 
     //se word è uguale a reverseWord è palindroma altrimenti no
-    return word == reverseWord ? "è palindroma" : "non è palindroma";
+    return word == reverseWord ? "La parola è palindroma" : "La parola non è palindroma";
 }
 
+//selezione bottone
+const verifyButton = document.querySelector("button");
 
-//risultato 
-const result = verificaParolaPalindroma(userWord)
-//(per test)
-console.log(result);
+//selezione output
+let verifyOutput = document.getElementById("output");
+
+//creo evento su click
+verifyButton.addEventListener("click",
+    function () {
+        //seleziono input per chiedere parola all'utente
+        const userWord = document.getElementById("user-word").value;
+
+        //comunico come output il risultato della verifa effettuata dalla funzione
+        verifyOutput.innerText = verificaParolaPalindroma(userWord);        
+    }
+)
