8800b8d7cdebaf2badec2006b3ad0a34328a59ef
battistaennio <ennio.battista99@gmail.com>
2024-06-12 16:41:54 +0200

    Feat: add pcNumber, userAdditionPc, resultEvenOdd

--- Pari e Dispari/main.js
+++ Pari e Dispari/main.js
@@ -15,20 +15,32 @@ function evenOdd(num) {
     return num % 2 === 0 ? "Pari" : "Dispari";
 }
 
-//utente sceglie se pari o dispari
+
+
+
+//utente sceglie se pari o dispari e numero
 const userEvenOdd = prompt("Scegli pari o dispari");
 console.log("user sceglie: ", userEvenOdd);
 
-// //utente sceglie numero
-// const userNumber = parseInt(prompt("Scegli un numero da 1 a 5"));
-// console.log(userNumber);
+const userNumber = parseInt(prompt("Scegli un numero da 1 a 5"));
+console.log("numero user:", userNumber);
 
-//condizione per far scegliere l'opposto di userEvenOdd
+
+//condizione per far scegliere l'opposto di userEvenOdd al PC e numero pc
 let pcEvenOdd = "";
 if (userEvenOdd === "pari") {
     pcEvenOdd = "dispari";
 } else if (userEvenOdd === "dispari") {
     pcEvenOdd ="pari"
 }
-
 console.log("il pc sceglie: ", pcEvenOdd);
+
+const pcNumber = randomNumber(1, 5)
+console.log("numero pc:", pcNumber);
+
+//creo somma di userNumber e pcNumber e verifico se è pari o dispari
+const userAdditionPc = userNumber + pcNumber;
+console.log("La somma dei numeri è: ", userAdditionPc);
+
+const resultEvenOdd = evenOdd(userAdditionPc);
+console.log(resultEvenOdd);
